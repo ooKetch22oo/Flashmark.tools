04d46afa4c8d25178cd7e78e7fbbfb85ddaab0a0
Neil Ketcham <ketch20.2@gmail.com>
2024-06-21 13:45:12 -0400

    Added a flex-col class to the profiler container to display the content in a vertical layout.

--- flashmark-tools-app/src/routes/profiler/index.tsx
+++ flashmark-tools-app/src/routes/profiler/index.tsx
@@ -42,7 +42,7 @@ return (
     <header class="flex gap-5 justify-between py-4 pr-5 pl-4 w-full border-b-4 border-black border-solid max-md:flex-wrap max-md:max-w-full ">
     <h1 class="my-auto text-3xl font-semibold leading-10 text-slate-950">.profiler</h1>
     </header>
-    <div class="flex flex-wrap pb-8 px-4 gap-4 grow row w-full rounded-lg overflow-auto max-h-[72svh]">
+    <div class="flex flex-col pb-8 px-4 gap-4 grow row w-full rounded-lg overflow-auto max-h-[72svh]">
     {Array.isArray(businesses.value) ? businesses.value.map((businesses: any) => (
 
           // <Link class="flex" key={businesses.id} href={`/profiler/${business}/${businesses.id}`}>
