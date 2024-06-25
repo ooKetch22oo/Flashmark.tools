268a4afd0770e2002b7d0310ba7082266386f88a
Neil Ketcham <ketch20.2@gmail.com>
2024-06-25 12:18:07 -0400

    Added a new footer component and integrated it into the layout.

---
+++ flashmark-tools-app/src/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+import { component$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
+
+export const Footer = component$(() => {
+  return (
+    <footer class="bg-gray-200 p-4 text-center">
+      <p>&copy; 2023 Our Platform. All rights reserved.</p>
+      <nav class="mt-2">
+        <Link href="/about" class="text-blue-600 hover:underline mx-2">About</Link>
+        <Link href="/contact" class="text-blue-600 hover:underline mx-2">Contact</Link>
+      </nav>
+    </footer>
+  );
+});
