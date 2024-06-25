0232de6f6f01c22c577813dfa42c8bf234bd3923
Neil Ketcham <ketch20.2@gmail.com>
2024-06-25 13:19:38 -0400

    Added a new React component for an authentication form.

---
+++ flashmark-tools-app/src/components/auth-form/auth-form.tsx
@@ -0,0 +1,13 @@
+import { component$ } from '@builder.io/qwik';
+
+export interface AuthFormProps {
+
+}
+
+export const AuthForm = component$<AuthFormProps>((props) => {
+  return (
+    <div>
+      AuthForm component works!
+    </div>
+  );
+});
